a05cd8255a1202247ed7000b9c5c3bc00a454800
Tony Fu <tonyfu.dev@gmail.com>
2022-03-23 17:22:07 +1100

    Update config

--- init.el
+++ init.el
@@ -5,9 +5,11 @@
 (require 'smartparens-config)
 
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+(fset 'yes-or-no-p 'y-or-n-p)
+
 
 (add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
-(setq default-frame-alist '((width . 150) (height . 50)))
+(setq default-frame-alist '((width . 135) (height . 40)))
 
 (global-set-key (kbd "M-i") 'imenu)
 (global-set-key (kbd "M-o") 'window-swap-states)
@@ -15,6 +17,7 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "<f1>") 'shell)
 (tool-bar-mode -1)
+(menu-bar-mode -1)
 (selectrum-mode +1)
 (setq completion-styles '(orderless))
 (savehist-mode)
@@ -47,7 +50,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 140 :family "Cascadia Code"))))
+ '(default ((t (:height 130 :family "Cascadia Code"))))
  '(whitespace-line ((t nil))))
 
 (add-hook 'prog-mode-hook
@@ -58,3 +61,5 @@
               (display-line-numbers-mode)
 	      (visual-line-mode))))
 (put 'dired-find-alternate-file 'disabled nil)
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
